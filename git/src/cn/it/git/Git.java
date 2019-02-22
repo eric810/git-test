9b20fbe3dd89049b6c94b06bcf6f4d10c7aa8264
eric <zcs93@qq.com>
2019-02-23 00:18:11 +0800

    s

---
+++ git/src/cn/it/git/Git.java
@@ -0,0 +1,9 @@
+package cn.it.git;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+        System.out.println("hello,git!");
+
+    }
+}
