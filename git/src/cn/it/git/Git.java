d24993796cd312d18e1603115ff6ab5e9ee1ed15
eric <zcs93@qq.com>
2019-02-23 00:28:47 +0800

    qqq

--- git/src/cn/it/git/Git.java
+++ git/src/cn/it/git/Git.java
@@ -4,11 +4,12 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hello,git!");
         System.out.println("hello,git!");
-
+        // hello test
     }
 
     public void test(){
         int a = 1;
         int b = 2;
+        System.out.println(a);
     }
 }
